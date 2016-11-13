885a0cd4d03ea8ba644bef50dd1f2331ba9d2a73
Andrew Wong <wongandrew97@gmail.com>
2016-11-12 22:39:03 -0500

    Bootstrap CSS and passing data through forms works now.

--- pathhero/main/views.py
+++ pathhero/main/views.py
@@ -9,5 +9,21 @@ import json, os
 def index(request):
 	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 	MAPS_KEY = json.loads(open(BASE_DIR + '/config.json', 'rb').read())['MAPS_KEY']
-	form = LocationInputForm()
-	return render(request, 'main/index.html', {'MAPS_KEY': MAPS_KEY, 'form': form})
+
+	origin = ""
+	dest = ""
+	if request.method == 'POST':
+		form = LocationInputForm(request.POST)
+		if form.is_valid():
+			origin = form.cleaned_data['origin']
+			dest = form.cleaned_data['destination']
+	else:
+		form = LocationInputForm()
+
+	load = {
+		'MAPS_KEY': MAPS_KEY,
+		'form': form,
+		'origin': origin,
+		'destination': dest
+	}
+	return render(request, 'main/index.html', load)
